c041c9725051027d3e054d3ae0d3336e5b13853a
Clara Musenya Musyoka <CMMUSYOKA@safaricom.co.ke>
2022-10-08 12:44:34 +0300

    Add the other varibles of using DOM

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 const femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
 
+//Getting input from user and validation
+function userValidation(){
+    var year= document.getElementById("year").value;
+    var century=parseInt(year.substring(0,2)) ;
+    var month= parseInt(document.getElementById("month").value);
+    var day= parseInt(document.getElementById("Day").value);
+    var gender = document.getElementsByName("Gender");
+}
