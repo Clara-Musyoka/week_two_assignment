39bab681a2a82553d21eabe3537136cadace9808
Clara Musenya Musyoka <CMMUSYOKA@safaricom.co.ke>
2022-10-08 13:01:50 +0300

    Add the function to calaculate the day user was born

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,22 @@ const dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday",
 const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 const femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
-
+//function to output name and day
+function calculateNameOfDay(century,year, month,day gender){
+    var indexOfTheDay= Math.round(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
+    var dayTheUserWasBorn =dayNames[indexOfTheDay];
+    var myName =null;
+    if ((gender[0].checked == true)){
+        myName=maleNames[indexOfTheDay];
+    }else if (gender[1].checked == true){
+       myName=femaleNames[indexOfTheDay];
+    }else
+    {
+        alert('invalid gender');
+    }
+var list= [dayTheUserWasBorn, myName];
+    return list;
+}
 //Getting input from user and validation
 function userValidation(){
     var year= document.getElementById("year").value;
